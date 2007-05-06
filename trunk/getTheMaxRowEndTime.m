a3a3c6c58fd9b7fe29f547bd6cebe2a9f502f3e7
jiing24 <jiing24@1e7ace29-2c2f-0410-a353-8113d636fefb>
2007-05-06 23:10:30 +0000

    almost conquered version

---
+++ trunk/getTheMaxRowEndTime.m
@@ -0,0 +1,17 @@
+function maxRowEndTime = getTheMaxRowEndTime(idx,col, sameJob,cTable)
+global timeTable
+global jobDealOrder  
+
+% maxRowEndTime denote the end time of previous element in the same row
+for cx = 1: col
+    for rx =1 : idx
+        if col == 1
+            maxRowEndTime = 0;
+        else
+            maxRowEndTime = timeTable{idx,col-1}.end;           
+        end
+    end
+end
+
+maxRowEndTime
+return
