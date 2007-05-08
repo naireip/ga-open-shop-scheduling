10b90773c72cddd735ea99a047e6c3e588bfc440
jiing24 <jiing24@1e7ace29-2c2f-0410-a353-8113d636fefb>
2007-05-08 22:50:17 +0000

    add orderMut

---
+++ trunk/orderMut.m
@@ -0,0 +1,27 @@
+%orderMut: order mutation for one chromosome
+%Randomly select two points and change the to point's gene
+%For example: 1234 -> 1432   pos=(2,4)
+%For example: 1234 -> 3214   pos=(1,3)
+function newVa=orderMut(Va)
+
+%fprintf('original Va is:\n')
+Va= [  8   7   4   2   6   5   1   9   3  10 ]
+original_Va=Va;
+
+Ranpoint=[]; %initialize Ranpoint
+
+while(length(Ranpoint)<2)
+Ranpoint=sort(mod(floor(randn(1,2)*10),length(Va))+1);
+end
+
+   tempPtValue = Va(Ranpoint(1));
+   Ranpoint
+   
+   Va(Ranpoint(1)) = Va(Ranpoint(2));
+   Va(Ranpoint(2)) = tempPtValue;
+
+
+fprintf('After random mutation, Va is:\n')
+
+newVa = Va;
+return
